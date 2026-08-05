be509ec9d1e80848d4cb2829d2179239bcec1a93
Jhonatan L. Santos <jhonatanlucassantosofc@gmail.com>
2026-08-05 10:24:07 -0300

    Bump version from 2.0.0 to 4.0.0

--- BeaverSec-main/beaversec/__init__.py
+++ BeaverSec-main/beaversec/__init__.py
@@ -2,7 +2,7 @@
 BeaverSec - Ferramenta modular de segurança ofensiva.
 """
 
-__version__ = "2.0.0"
+__version__ = "4.0.0"
 __all__ = ["Beaver", "BaseModule"]
 
 from beaversec.core.beaver import Beaver
